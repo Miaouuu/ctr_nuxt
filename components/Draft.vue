11f0fe79763f8922c8c154da0b112eb80e8809d6
mathieught <mathieu.ght@gmail.com>
2020-05-18 17:15:24 +0200

    change timer svg

--- components/Draft.vue
+++ components/Draft.vue
@@ -99,7 +99,6 @@
 
     <div class="timer">
       <svg>
-        <!-- Polygon en dehors de l'ecran, à replacer -->
         <polygon points="0 0 18 36.5 230 36.5 255 0 0 0" fill="#FAC600" />
       </svg>
       <h1 class="timerTxt">{{ timeLeft }}</h1>
